580b972d2481045d6152445559a2ec6a49082d8a
Kaung Nyan Lin <darkliyznyanlin910@gmail.com>
2025-08-18 06:43:31 +0800

    fixing dependency

--- orms/db-auth/scripts/db-seed.ts
+++ orms/db-auth/scripts/db-seed.ts
@@ -1,37 +1,7 @@
 import { hashPassword } from "better-auth/crypto";
-import { eq, inArray } from "drizzle-orm";
-
-import { initAuth } from "@repo/auth-common";
 
 import { db } from "../src/client";
-import {
-  accounts,
-  members,
-  oauthApplications,
-  organizations,
-  users,
-} from "../src/schema";
-
-export const auth = initAuth(db, {
-  baseUrl: "http://localhost:3000/auth",
-  secret: process.env.AUTH_SECRET,
-  getOrganizations: async (userId) => {
-    const orgs = await db.query.members.findMany({
-      where: eq(members.userId, userId),
-    });
-    const orgSlugs = await db.query.organizations.findMany({
-      columns: {
-        slug: true,
-      },
-      where: inArray(
-        organizations.id,
-        orgs.map((org) => org.organizationId),
-      ),
-    });
-    return orgSlugs.map((org) => org.slug ?? "");
-  },
-  baseDomain: "localhost",
-});
+import { accounts, members, organizations, users } from "../src/schema";
 
 await db.transaction(async (tx) => {
   await tx
@@ -55,49 +25,25 @@ await db.transaction(async (tx) => {
       updatedAt: new Date(),
     })
     .onConflictDoNothing();
-});
 
-const res = await auth.api.signInEmail({
-  body: {
-    email: "admin@test.com",
-    password: "password",
-  },
-});
-
-const headers = new Headers({ Authorization: `Bearer ${res.token}` });
-
-try {
-  await auth.api.checkOrganizationSlug({
-    headers,
-    body: {
-      slug: "admin-org",
-    },
-  });
-  console.log("Organization not found, creating...");
-  await auth.api.createOrganization({
-    headers,
-    body: {
+  const orgId = "admin-org";
+  await tx
+    .insert(organizations)
+    .values({
+      id: orgId,
       name: "Admin Org",
       slug: "admin-org",
-    },
-  });
-} catch (e) {
-  console.log("Organization already exists, skipping...");
-}
+      createdAt: new Date(),
+    })
+    .onConflictDoNothing();
 
-await db
-  .insert(oauthApplications)
-  .values({
-    id: "nS3fF8G3CXnkgxuMxTdPQonqrp0hzmZ2",
-    name: "ArgoCD",
-    // DO NOT CHANGE THESE VALUES for the ArgoCD application to work
-    clientId: "EOfMkngqCQmPeaaIQjrwHQHNIUyjPOdS",
-    clientSecret: "VvrMhfMJBjDHMDWNTetIQGkNykfrmPfb",
-    redirectURLs: "http://argocd.127.0.0.1.nip.io/auth/callback",
-    type: "web",
-    disabled: false,
+  await tx.insert(members).values({
+    id: "admin-org-member",
+    organizationId: orgId,
     userId: "admin",
-  })
-  .onConflictDoNothing();
+    createdAt: new Date(),
+    role: "owner",
+  });
+});
 
 process.exit(0);
